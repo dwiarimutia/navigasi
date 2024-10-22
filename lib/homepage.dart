a7fd4c9e575aa79feeb3e837681d5409c413b454
DwiariMutia <dwiarimutia507@gmail.com>
2024-10-22 17:21:32 +0800

    menambahkan konten

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,17 +9,54 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Halaman Utama'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_forward, size: 40, color: Colors.blueAccent,), 
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Secondpage()), 
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: IconButton(
-          
-          icon: Icon(Icons.arrow_forward, size: 40, color: Colors.blueAccent,), 
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Secondpage()), 
-            );
-          },
+      body:Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.redAccent,
+          ),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,  // 2 item per baris
+              crossAxisSpacing: 10.0, // Jarak antar kolom
+              mainAxisSpacing: 10.0,  // Jarak antar baris
+              childAspectRatio: 1.0,  // Rasio membuat grid persegi
+            ),
+            itemCount: 4,
+            itemBuilder: (context, index){
+                return InkWell(
+                onTap: () {
+                  // Aksi ketika item di klik
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Secondpage()), // Navigasi ke halaman lain
+                  );
+                },
+                child: Container(
+                color: Colors.indigo,
+                child: Center(
+                  child: Text(
+                    'Item ${index + 1}', // Menampilkan item keberapa
+                    style: const TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                )
+              );
+            }
+          ),
         ),
       ),
     );
